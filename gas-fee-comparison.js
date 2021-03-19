b0e0263f7803a836161c17e679413ad2e61345ce
bguiz <bguiz@users.noreply.github.com>
2021-03-19 18:28:52 +0800

    feat: query coin-fiat rate and calculate fiat cost of transaction

--- gas-fee-comparison.js
+++ gas-fee-comparison.js
@@ -52,6 +52,49 @@ async function getGasPrice(web3Instance) {
   return web3Instance.eth.getGasPrice();
 }
 
+async function getCoinPrice({ coinSymbol, fiatSymbol }) {
+  const url =
+    `https://min-api.cryptocompare.com/data/price?fsym=${coinSymbol}&tsyms=${fiatSymbol}`;
+
+  const response = await axios({
+    method: 'get',
+    url,
+    responseType: 'json',
+    responseEncoding: 'utf8',
+  });
+  return response.data;
+}
+
+function augmentWithFees({
+  name,
+  web3Instance,
+  gasUsed,
+  gasPrice,
+  coinPair,
+  decimalPoints = 18,
+}) {
+  const fiatRateInCents = web3Instance.utils.toBN(
+    Math.floor(coinPair.USD * 100),
+  );
+  const decimalPointMultiplier = web3Instance.utils.toBN(
+    Math.pow(10, decimalPoints),
+  );
+  const cryptoFee = web3Instance.utils.toBN(gasUsed)
+    .mul(web3Instance.utils.toBN(gasPrice));
+  const fiatFeeCents = cryptoFee
+    .mul(fiatRateInCents)
+    .div(decimalPointMultiplier);
+  const fiatFee = (fiatFeeCents.toNumber() / 100).toFixed(2);
+  return {
+    name,
+    gasUsed,
+    gasPrice,
+    coinPair,
+    cryptoFee: cryptoFee.toString(),
+    fiatFee,
+  };
+}
+
 async function executeAndLogForEach(
   list,
   names,
@@ -68,8 +111,8 @@ async function executeAndLogForEach(
 
 async function performComparison() {
   const names = [
-    'rsk/testnet',
-    'ethereum/kovan',
+    'rsk',
+    'ethereum',
   ];
 
   const web3ProviderRsk = initialiseRsk();
@@ -90,6 +133,7 @@ async function performComparison() {
   await executeAndLogForEach(
     web3Instances, names, 'blockNumber', getBlockNumber,
   );
+
   const rifTokenAddresses = [
     '0x19f64674d8a5b4e652319f5e239efd3bc969a1fe',
     '0x69f6d4d4813f8e2e618dae7572e04b6d5329e207',
@@ -97,6 +141,7 @@ async function performComparison() {
   await executeAndLogForEach(
     rifTokenAddresses, names, 'rifTokenContractAddress', ((x) => (x)),
   );
+
   const rifTokenDeploymentTxHashes = [
     '0xf56cd8264ada751fec85d2646fb593ab3b4cf53e8104a6e5768097239b5fe2eb',
     '0x1ecb1e45f7eb27e9fa4a43d2d71d37e92d1a1789b7b1bfbc4bf233598aeade52',
@@ -104,6 +149,7 @@ async function performComparison() {
   await executeAndLogForEach(
     rifTokenDeploymentTxHashes, names, 'rifTokenContractDeploymentTx', ((x) => (x)),
   );
+
   const gasUsedPromises = await executeAndLogForEach(
     [
       { web3Instance: web3Instances[0], txHash: rifTokenDeploymentTxHashes[0], },
@@ -114,12 +160,29 @@ async function performComparison() {
   const gasPricePromises = await executeAndLogForEach(
     web3Instances, names, 'currentGasPrice', getGasPrice,
   );
+
+  const coinPairs = [
+    { coinSymbol: 'BTC', fiatSymbol: 'USD', },
+    { coinSymbol: 'ETH', fiatSymbol: 'USD', },
+  ];
+  const coinPairPromises = await executeAndLogForEach(
+    coinPairs, names, 'coinPrice', getCoinPrice,
+  );
   const gasUsedResults = await Promise.all(gasUsedPromises);
   const gasPriceResults = await Promise.all(gasPricePromises);
-  console.log({
-    gasUsedResults,
-    gasPriceResults,
-  });
+  const coinPairResults = await Promise.all(coinPairPromises);
+  const results = names
+    .map((name, idx) => {
+      return {
+        name,
+        web3Instance: web3Instances[idx],
+        gasUsed: gasUsedResults[idx],
+        gasPrice: gasPriceResults[idx],
+        coinPair: coinPairResults[idx],
+      };
+    })
+    .map(augmentWithFees);
+  console.log('erc20 deployment (RIF token)', results);
 }
 
 performComparison();
